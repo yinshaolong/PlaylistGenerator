2cc25b7e05d6c3df3e5bcec6d92a0f44e90a638e
omgitsjohn <syin18@my.bcit.ca>
2023-11-17 19:19:28 -0800

    fixed bug tracks was not list in get_playlist_song

--- app.py
+++ app.py
@@ -35,16 +35,21 @@ def create_empty_playlist(spotify:object, current_user, title:str): #title == us
     )
     return created_playlist
 
-def get_playlist_songs(created_playlist, search_results = None):
+def get_playlist_songs(spotify, search_results = None):
     '''returns a list of song ids from the search results'''
-    results_len = len(search_results["tracks"]["items"])
-    tracks = search_results["tracks"]["items"][0]["id"]
+    # results_len = len(search_results["tracks"]["items"])
+    print(len(search_results), search_results)
+    tracks = [search_results["tracks"]["items"][0]["id"]]
+    # search_results = spotify.search(q="Uptown Funk", type = "track", limit=10)
+    # results_len = len(search_results["tracks"]["items"])
+    # tracks = ([search_results["tracks"]["items"][num]["id"] for num in range(results_len)])
+    print(tracks)
     return tracks
 
 def get_search_queries(spotify, playlist):
     '''returns a list spotify.search object for each song in the playlist'''
     search_queries = get_song_titles(playlist)
-    search_results = spotify.search(q=search_queries, type = "track", limit=10)
+    search_results = spotify.search(q=search_queries, type = "track", limit=len(search_queries))
     return search_results
 
 def generate_playlist(length = None, prompt = None):
@@ -54,7 +59,7 @@ def generate_playlist(length = None, prompt = None):
     current_user = spotify.current_user()
     created_playlist = create_empty_playlist(spotify, current_user, user_prompt)
     search_queries = get_search_queries(spotify, playlist)
-    tracks = get_playlist_songs(created_playlist, search_queries)
+    tracks = get_playlist_songs(spotify, search_queries)
     spotify.user_playlist_add_tracks(current_user["id"], created_playlist["id"], tracks)
 
 #delete .cache to log user out
@@ -62,10 +67,7 @@ def generate_playlist(length = None, prompt = None):
 
 
 # assert current_user is not Noney
-# #searchs for 10 songs with the name "Uptown Funk" and prints their ids
-# search_results = spotify.search(q="Uptown Funk", type = "track", limit=10)
-# results_len = len(search_results["tracks"]["items"])
-# tracks = ([search_results["tracks"]["items"][num]["id"] for num in range(results_len)])
+#searchs for 10 songs with the name "Uptown Funk" and prints their ids
 
 def main():
     generate_playlist()
