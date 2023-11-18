a49702a5f49c57d7bfc12f801bfedd3d452ad8ff
omgitsjohn <syin18@my.bcit.ca>
2023-11-17 19:02:50 -0800

    made funcs for creating a user playlist and getting list of song ids

--- app.py
+++ app.py
@@ -1,39 +1,43 @@
 import spotipy
 from dotenv import dotenv_values
 from playlist_generator import get_playlist
-config = dotenv_values(".env")
 
-playlist, user_prompt = get_playlist()
-user_prompt = " ".join(user_word.capitalize() for user_word in user_prompt.split(" "))
-print(user_prompt)
 
-spotify = spotipy.Spotify(
-    auth_manager=spotipy.SpotifyOAuth(
-        client_id=config["SPOTIFY_CLIENT_ID"],
-        client_secret=config["SPOTIFY_CLIENT_SECRET"],
-        redirect_uri="http://localhost:9999",
-        #determines what a user can do after logging in
-        scope="playlist-modify-private",
-        # scope="playlist-modify-public"
+def get_song_titles(playlist):
+    search_queries = []
+    for song in playlist:
+        search_queries.append(f"{song['song']} {song['artist']}")
+    return search_queries
+
+def capitalize_prompt(user_prompt):
+    user_prompt = " ".join(user_word.capitalize() for user_word in user_prompt.split(" "))
+    return user_prompt
+
+def auth_spotify(playlist_scope = "private"):
+    config = dotenv_values(".env")
+    spotify = spotipy.Spotify(
+        auth_manager=spotipy.SpotifyOAuth(
+            client_id=config["SPOTIFY_CLIENT_ID"],
+            client_secret=config["SPOTIFY_CLIENT_SECRET"],
+            redirect_uri="http://localhost:9999",
+            #determines what a user can do after logging in
+            scope="playlist-modify-private" if playlist_scope == "private" else "playlist-modify-public"
+        )
     )
-)
+    return spotify
 
-current_user = spotify.current_user()
-# print(current_user)
 
-#delete .cache to log user out
+def create_empty_playlist(spotify:object, current_user, title:str): #title == user_prompt
+    created_playlist = spotify.user_playlist_create(
+        current_user["id"],
+        public=False,
+        name = title
+    )
+    return created_playlist
 
-assert current_user is not None
-#searchs for 10 songs with the name "Uptown Funk" and prints their ids
-search_results = spotify.search(q="Uptown Funk", type = "track", limit=10)
-results_len = len(search_results["tracks"]["items"])
-tracks = ([search_results["tracks"]["items"][num]["id"] for num in range(results_len)])
+def get_playlist_songs(created_playlist, search_results = None):
+    '''returns a list of song ids from the search results'''
+    results_len = len(search_results["tracks"]["items"])
+    tracks = search_results["tracks"]["items"][0]["id"]
+    return tracks
 
-#creates playlist
-created_playlist = spotify.user_playlist_create(
-    current_user["id"],
-    public=False,
-    name = user_prompt
-)
-
-spotify.user_playlist_add_tracks(current_user["id"], created_playlist["id"], tracks)
