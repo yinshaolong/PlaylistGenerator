0c485fbbf088abcb5de864507353dc3c4983848e
omgitsjohn <syin18@my.bcit.ca>
2023-11-17 17:01:29 -0800

    creating a default playlist without any songs

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import spotipy
 from dotenv import dotenv_values
+from playlist_generator import get_playlist
 
 config = dotenv_values(".env")
 
@@ -7,9 +8,27 @@ spotify = spotipy.Spotify(
     auth_manager=spotipy.SpotifyOAuth(
         client_id=config["SPOTIFY_CLIENT_ID"],
         client_secret=config["SPOTIFY_CLIENT_SECRET"],
-        redirect_uri="http://localhost:9999"
+        redirect_uri="http://localhost:9999",
+        #determines what a user can do after logging in
+        scope="playlist-modify-private",
+        # scope="playlist-modify-public"
     )
 )
 
 current_user = spotify.current_user()
-print(current_user)
+# print(current_user)
+
+#delete .cache to log user out
+
+assert current_user is not None
+#searchs for 10 songs with the name "Uptown Funk" and prints their ids
+search_results = spotify.search(q="Uptown Funk", type = "track", limit=10)
+results_len = len(search_results["tracks"]["items"])
+print([search_results["tracks"]["items"][num]["id"] for num in range(results_len)])
+
+#creates playlist
+spotify.user_playlist_create(
+    current_user["id"],
+    public=False,
+    name="Testing Playlist"
+)
